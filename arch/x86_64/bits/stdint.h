f3a8faa9ff00f07da2c167e13c78d38555da5bdc
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-06-17 11:01:10 +0100

    feat: Addition of 64-bit types to the x86_64 architecture.

---
+++ arch/x86_64/bits/stdint.h
@@ -0,0 +1,24 @@
+/**
+ * @file arch/x86_64/bits/stdint.h
+ * @brief Defines integer types for x86_64 architecture.
+ *
+ * Provides typedefs for signed and unsigned integers.
+ *
+ * @author Ismael Moreira
+ */
+#ifndef __CILIBC__X86_64__STANDARD_INTEGER__
+#define __CILIBC__X86_64__STANDARD_INTEGER__
+
+/**
+ * @typedef _Int64
+ * @brief Signed 64-bit integer type.
+ */
+typedef signed long _Int64;
+
+/**
+ * @typedef _Uint64
+ * @brief Unsigned 64-bit integer type.
+ */
+typedef unsigned long _Uint64;
+
+#endif // __CILIBC__X86_64__STANDARD_INTEGER__
