4c9c17966e90af44079e78f42e95ff6d3be3e668
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-06-18 22:26:06 +0100

    feat: Addition of new flags in the automation script to prevent gcc from inferring macros and stdc-predef.h.

--- scripts/Compiler.py
+++ scripts/Compiler.py
@@ -52,6 +52,8 @@ class CCompiler:
             "-fno-builtin",
             "-fno-stack-protector",
             "-fno-PIC",
+            "-nostdinc",
+            "-undef",
             "-Wall",
         ]
 
@@ -62,6 +64,8 @@ class CCompiler:
             "-fno-builtin",
             "-fno-stack-protector",
             "-fno-PIC",
+            "-nostdinc",
+            "-undef",
             "-Wall",
         ]
 
