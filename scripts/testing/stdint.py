a9fdb7f35ad419e5739adba12de3bad29c6d3c10
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-06-19 05:17:47 +0100

    test: Addition of the test that checks if INTPTR_MIN is well defined.

--- scripts/testing/stdint.py
+++ scripts/testing/stdint.py
@@ -640,6 +640,22 @@ class TestStdint(unittest.TestCase):
         self.assertEqual(stdout.strip(), "")
         self.assertEqual(stderr, "")
 
+    def testIntPtrMin(self):
+        """
+        Test that INTPTR_MIN is -9223372036854775808.
+        """
+        result: CompletedProcess = run(
+            ["__build/test/intptrmin"], capture_output=True, text=True
+        )
+
+        stdout: Any = result.stdout
+        stderr: Any = result.stderr
+        exitCode: int = result.returncode
+
+        self.assertEqual(exitCode, 0)
+        self.assertEqual(stdout.strip(), "")
+        self.assertEqual(stderr, "")
+
 
 if __name__ == "__main__":
     unittest.main()
