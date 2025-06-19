798de6715e7498a28a0580e5b9aaf06103a4ee84
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-06-19 05:39:58 +0100

    test: Addition of the test that checks if PTRDIFF_MIN is well defined.

--- scripts/testing/stdint.py
+++ scripts/testing/stdint.py
@@ -688,6 +688,22 @@ class TestStdint(unittest.TestCase):
         self.assertEqual(stdout.strip(), "")
         self.assertEqual(stderr, "")
 
+    def testPtrDiffMin(self):
+        """
+        Test that PTRDIFF_MIN is -9223372036854775808.
+        """
+        result: CompletedProcess = run(
+            ["__build/test/ptrdiffmin"], capture_output=True, text=True
+        )
+
+        stdout: Any = result.stdout
+        stderr: Any = result.stderr
+        exitCode: int = result.returncode
+
+        self.assertEqual(exitCode, 0)
+        self.assertEqual(stdout.strip(), "")
+        self.assertEqual(stderr, "")
+
 
 if __name__ == "__main__":
     unittest.main()
---
+++ tests/stdint/ptrdiff/ptrdiffmin.test.c
@@ -0,0 +1,34 @@
+/**
+ * @file tests/stdint/ptrdiff/ptrdiffmin.test.c
+ * @brief Test for the ptrdiff_t minimum value.
+ *
+ * This test checks that the PTRDIFF_MIN macro is defined correctly
+ * and that it equals -9223372036854775808, which is the minimum value
+ * for a ptrdiff_t type, a signed 64-bit integer.
+ *
+ * @see https://pubs.opengroup.org/onlinepubs/9799919799/
+ * @see include/stdint.h
+ * @see arch/__your_arch__/bits/stdint.h
+ *
+ * @author Ismael Moreira
+ */
+#include "stdint.h"
+
+/**
+ * @brief Main function to test PTRDIFF_MIN.
+ *
+ * This function checks if PTRDIFF_MIN is equal to -9223372036854775808.
+ * If it is not, the function returns 1 indicating a failure.
+ * If it is, the function returns 0 indicating success.
+ *
+ * @return int Returns 0 on success, 1 on failure.
+ */
+int main(void)
+{
+    ptrdiff_t test_value = -9223372036854775808;
+
+    if (PTRDIFF_MIN != test_value)
+        return 1;
+
+    return 0;
+}
