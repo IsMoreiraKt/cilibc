0f50c642119c4bc97cdf20d2e2dd2b51b2bcf7c4
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-06-19 02:07:03 +0100

    test: Addition of the test that checks if UINT_LEAST8_MAX is well defined.

--- scripts/testing/stdint.py
+++ scripts/testing/stdint.py
@@ -336,6 +336,22 @@ class TestStdint(unittest.TestCase):
         self.assertEqual(stdout.strip(), "")
         self.assertEqual(stderr, "")
 
+    def testUIntLeast8Max(self):
+        """
+        Test that UINT_LEAST8_MAX is 255.
+        """
+        result: CompletedProcess = run(
+            ["__build/test/uintleast8max"], capture_output=True, text=True
+        )
+
+        stdout: Any = result.stdout
+        stderr: Any = result.stderr
+        exitCode: int = result.returncode
+
+        self.assertEqual(exitCode, 0)
+        self.assertEqual(stdout.strip(), "")
+        self.assertEqual(stderr, "")
+
 
 if __name__ == "__main__":
     unittest.main()
