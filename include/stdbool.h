87716079291ed147a55a48854c284b773a470118
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-06-20 00:13:08 +0100

    feat: Addition of the bool type that serves as an alias for the _Bool type defined by the compiler.

---
+++ include/stdbool.h
@@ -0,0 +1,22 @@
+/**
+ * @file include/stdbool.h
+ * @brief Standard boolean type definitions.
+ * 
+ * Defines the boolean type and its values for the CILibc.
+ * 
+ * @see https://pubs.opengroup.org/onlinepubs/9799919799/
+ * 
+ * @author Ismael Moreira
+ */
+#ifndef __CILIBC__STANDARD_BOOLEAN__
+#define __CILIBC__STANDARD_BOOLEAN__
+
+/**
+ * @typedef bool
+ * @brief Boolean type.
+ * 
+ * This type is used to represent boolean values in CILibc.
+ */
+typedef _Bool bool;
+
+#endif // __CILIBC__STANDARD_BOOLEAN__
