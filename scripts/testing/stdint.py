2bc5123c29d0a0bb3c31e8418f81593397ed9344
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-06-19 03:52:25 +0100

    test: Addition of the test that checks if INT_FAST16_MAX is well defined.

--- scripts/testing/stdint.py
+++ scripts/testing/stdint.py
@@ -480,6 +480,22 @@ class TestStdint(unittest.TestCase):
         self.assertEqual(stdout.strip(), "")
         self.assertEqual(stderr, "")
 
+    def testIntFast16Max(self):
+        """
+        Test that INT_FAST16_MAX is 32767.
+        """
+        result: CompletedProcess = run(
+            ["__build/test/intfast16max"], capture_output=True, text=True
+        )
+
+        stdout: Any = result.stdout
+        stderr: Any = result.stderr
+        exitCode: int = result.returncode
+
+        self.assertEqual(exitCode, 0)
+        self.assertEqual(stdout.strip(), "")
+        self.assertEqual(stderr, "")
+
 
 if __name__ == "__main__":
     unittest.main()
