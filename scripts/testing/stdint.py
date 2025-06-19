e72ac72c8adb511ce6be7eae3e4bb9e08ffb0337
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-06-19 01:37:42 +0100

    test: Addition of the test that checks if INT_LEAST32_MIN is well defined.

--- scripts/testing/stdint.py
+++ scripts/testing/stdint.py
@@ -240,6 +240,22 @@ class TestStdint(unittest.TestCase):
         self.assertEqual(stdout.strip(), "")
         self.assertEqual(stderr, "")
 
+    def testIntLeast32Min(self):
+        """
+        Test that INT_LEAST32_MIN is -2147483648.
+        """
+        result: CompletedProcess = run(
+            ["__build/test/intleast32min"], capture_output=True, text=True
+        )
+
+        stdout: Any = result.stdout
+        stderr: Any = result.stderr
+        exitCode: int = result.returncode
+
+        self.assertEqual(exitCode, 0)
+        self.assertEqual(stdout.strip(), "")
+        self.assertEqual(stderr, "")
+
 
 if __name__ == "__main__":
     unittest.main()
