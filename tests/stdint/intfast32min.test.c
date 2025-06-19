652573214a39a2df4bdabd97d5fad45a14215a60
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-06-19 02:37:49 +0100

    test: Addition of the test that checks if INT_FAST32_MIN is well defined.

---
+++ tests/stdint/intfast32min.test.c
@@ -0,0 +1,31 @@
+/**
+ * @file tests/stdint/intfast32min.test.c
+ * @brief Test for the int_fast32_t minimum value.
+ *
+ * This test checks that the INT_FAST32_MIN macro is defined correctly
+ * and that it equals -2147483648, which is the minimum value for a signed
+ * 32-bit integer.
+ *
+ * @author Ismael Moreira
+ */
+#include "stdint.h"
+#include "tests.h"
+
+/**
+ * @brief Main function to test INT_FAST32_MIN.
+ *
+ * This function checks if INT_FAST32_MIN is equal to -2147483648.
+ * If it is not, the function returns 1 indicating a failure.
+ * If it is, the function returns 0 indicating success.
+ *
+ * @return int Returns 0 on success, 1 on failure.
+ */
+int main(void)
+{
+    int_fast32_t test_value = -2147483648;
+
+    if (INT_FAST32_MIN != test_value)
+        return 1;
+
+    return 0;
+}
