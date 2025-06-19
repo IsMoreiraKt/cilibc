a450c564092199f5acc420d0191276a2410ac35c
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-06-19 01:51:32 +0100

    test: Addition of the test that checks if INT_LEAST32_MAX is well defined.

--- scripts/testing/stdint.py
+++ scripts/testing/stdint.py
@@ -304,6 +304,22 @@ class TestStdint(unittest.TestCase):
         self.assertEqual(stdout.strip(), "")
         self.assertEqual(stderr, "")
 
+    def testIntLeast32Max(self):
+        """
+        Test that INT_LEAST32_MAX is 2147483647.
+        """
+        result: CompletedProcess = run(
+            ["__build/test/intleast32max"], capture_output=True, text=True
+        )
+
+        stdout: Any = result.stdout
+        stderr: Any = result.stderr
+        exitCode: int = result.returncode
+
+        self.assertEqual(exitCode, 0)
+        self.assertEqual(stdout.strip(), "")
+        self.assertEqual(stderr, "")
+
 
 if __name__ == "__main__":
     unittest.main()
---
+++ tests/stdint/intleast32max.test.c
@@ -0,0 +1,31 @@
+/**
+ * @file tests/stdint/intleast32max.test.c
+ * @brief Test for INT_LEAST32_MAX.
+ *
+ * This test checks that the INT_LEAST32_MAX macro is defined correctly
+ * and that it equals 2147483647, which is the maximum value for a signed
+ * 32-bit integer.
+ *
+ * @author Ismael Moreira
+ */
+#include "stdint.h"
+#include "tests.h"
+
+/**
+ * @brief Main function to test INT_LEAST32_MAX.
+ *
+ * This function checks if INT_LEAST32_MAX is equal to 2147483647.
+ * If it is not, the function returns 1 indicating a failure.
+ * If it is, the function returns 0 indicating success.
+ *
+ * @return int Returns 0 on success, 1 on failure.
+ */
+int main(void)
+{
+    int_least32_t test_value = 2147483647;
+
+    if (INT_LEAST32_MAX != test_value)
+        return 1;
+
+    return 0;
+}
