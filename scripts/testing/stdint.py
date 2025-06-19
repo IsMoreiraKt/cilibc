47f18aa890ec2ac6582fec346229dbba0ebd282e
IsMoreiraKt <ismaelmoreirakt@gmail.com>
2025-06-19 01:39:40 +0100

    test: Addition of the test that checks if INT_LEAST64_MIN is well defined.

--- scripts/testing/stdint.py
+++ scripts/testing/stdint.py
@@ -256,6 +256,22 @@ class TestStdint(unittest.TestCase):
         self.assertEqual(stdout.strip(), "")
         self.assertEqual(stderr, "")
 
+    def testIntLeast64Min(self):
+        """
+        Test that INT_LEAST64_MIN is -9223372036854775808.
+        """
+        result: CompletedProcess = run(
+            ["__build/test/intleast64min"], capture_output=True, text=True
+        )
+
+        stdout: Any = result.stdout
+        stderr: Any = result.stderr
+        exitCode: int = result.returncode
+
+        self.assertEqual(exitCode, 0)
+        self.assertEqual(stdout.strip(), "")
+        self.assertEqual(stderr, "")
+
 
 if __name__ == "__main__":
     unittest.main()
---
+++ tests/stdint/intleast64min.test.c
@@ -0,0 +1,31 @@
+/**
+ * @file tests/stdint/intleast64min.test.c
+ * @brief Test for INT_LEAST64_MIN.
+ *
+ * This test checks that the INT_LEAST64_MIN macro is defined correctly
+ * and that it equals -9223372036854775808, which is the minimum value
+ * for a signed 64-bit integer.
+ *
+ * @author Ismael Moreira
+ */
+#include "stdint.h"
+#include "tests.h"
+
+/**
+ * @brief Main function to test INT_LEAST64_MIN.
+ *
+ * This function checks if INT_LEAST64_MIN is equal to -9223372036854775808.
+ * If it is not, the function returns 1 indicating a failure.
+ * If it is, the function returns 0 indicating success.
+ *
+ * @return int Returns 0 on success, 1 on failure.
+ */
+int main()
+{
+    int64_t test_value = -9223372036854775808;
+
+    if (INT_LEAST64_MIN != test_value)
+        return 1;
+
+    return 0;
+}
